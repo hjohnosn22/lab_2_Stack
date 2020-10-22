5e53a318365a16a7d4a58f3bd0fb2d5034e4f1f5
Henry Johnson <46233024+HunkyHank1@users.noreply.github.com>
2020-10-22 13:22:48 -0600

    I have copied in my node class and outlined construcors, accessors and mutators

--- src/Stack.java
+++ src/Stack.java
@@ -1,2 +1,20 @@
 public class Stack {
+    public Stack(){
+
+    }
+    public void push(){
+
+    }
+    public StackNode pop(){
+
+    }
+    public StackNode peek(){
+
+    }
+    public int size(){
+
+    }
+    public boolean isEmpty(){
+
+    }
 }
